e925af377f568f3b884097cdbed3c70140217a06
truonguy <truonguy123tx@gmail.com>
2024-09-11 11:33:22 +0700

    Generate Models, Migrations, Model Filable & Realtions, Factories, Seed Data

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Conversation;
+use App\Models\Group;
+use App\Models\Message;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true,
+        ]);
+
+        User::factory()->create([
+            'name' => 'Jane Doe',
+            'email' => 'jane@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        User::factory(10)->create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $group = Group::factory()->create([
+                'owner_id' => 1,
+            ]);
+            $users = User::inRandomOrder()->limit(rand(2, 5))->pluck('id');
+            $group->users()->attach(array_unique([1, ...$users]));
+        }
+
+        Message::factory(1000)->create();
+        $messages = Message::WhereNull('group_id')->orderBy('created_at')->get();
+
+        $conversations = $messages->groupBy(function ($message) {
+            return collect([$message->sender_id, $message->receiver_id])->sort()->implode('-');
+        })->map(function ($groupMessages) {
+            return [
+                'user_id1' => $groupMessages->first()->sender_id,
+                'user_id2' => $groupMessages->first()->receiver_id,
+                'last_message_id' => $groupMessages->last()->id,
+                'created_at' => $groupMessages->last()->created_at,
+                'updated_at' => $groupMessages->last()->created_at,
+            ];
+        })->values();
+
+        Conversation::insertOrIgnore($conversations->toArray());
+    }
+}
